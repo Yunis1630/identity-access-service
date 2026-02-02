d20b9874ef3de98ffca64cbd2c9452c2120a4463
Yunis Esgerov <esgeresgerov965@gmail.com>
2026-02-02 14:59:00 +0400

    first commit

---
+++ src/test/java/com/test/identity_access_service/IdentityAccessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.identity_access_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityAccessServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
