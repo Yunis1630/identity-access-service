59080257b36c378b05b442c5bfd5fb442b570f2e
Yunis Esgerov <esgeresgerov965@gmail.com>
2026-02-02 17:58:12 +0400

    just fix

--- src/main/java/com/test/identity_access_service/config/GeneralConfig.java
+++ src/main/java/com/test/identity_access_service/config/GeneralConfig.java
@@ -1,9 +1,11 @@
 package com.test.identity_access_service.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 public class GeneralConfig {
 
     @Bean
